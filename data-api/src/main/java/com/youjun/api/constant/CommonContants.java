dd84aba0c7a8f711e715edffb4022dd17ed3ec75
xuzhipeng <xuzhipengaly>
2021-09-17 14:36:29 +0800

    报错修改

--- data-api/src/main/java/com/youjun/api/constant/CommonContants.java
+++ data-api/src/main/java/com/youjun/api/constant/CommonContants.java
@@ -2,7 +2,7 @@ package com.youjun.api.constant;
 
 /**
  * <p>
- *  常量配置
+ * 常量配置
  * </p>
  *
  * @author kirk
@@ -10,14 +10,61 @@ package com.youjun.api.constant;
  */
 public class CommonContants {
     /**
+     * 超级管理员用户名称
+     */
+    public final static String ADMIN = "admin";
+
+    /**
+     * 验证码
+     */
+    public static final String CAPTCHA = "captcha";
+    /**
+     * 初始化密码
+     */
+    public static final String INITIAL_PASSWORD = "111111";
+    /**
+     * 定时任务用户名
+     */
+    public static final String SCHEDULE = "schedule";
+    /**
      * APP更新
      */
-    public final static String APP_UPGRADE_ENABLED="upgradeEnabled";
-    public final static String APP_LATEST_VERSION="appLatestVersion";
-    public final static String APP_ANDROID_PKGURL="androidPkgUrl";
-    public final static String APP_IOS_PKGURL="iOSPkgUrl";
-    public final static String APP_ANDROID_TYPE="android";
-    public final static String APP_IOS_TYPE="ios";
-    public final static String APP_WGTURL_KEY_PREFIX="wgtUrl-";
-    public final static String APP_WGT_OSS_FOLDER ="upgrade/";
+    public final static String APP_UPGRADE_ENABLED = "upgradeEnabled";
+    public final static String APP_LATEST_VERSION = "appLatestVersion";
+    public final static String APP_ANDROID_PKGURL = "androidPkgUrl";
+    public final static String APP_IOS_PKGURL = "iOSPkgUrl";
+    public final static String APP_ANDROID_TYPE = "android";
+    public final static String APP_IOS_TYPE = "ios";
+    public final static String APP_WGTURL_KEY_PREFIX = "wgtUrl-";
+    public final static String APP_WGT_OSS_FOLDER = "upgrade/";
+
+    /**
+     * 成功
+     */
+    public static final boolean SUCCESS_BOOL = true;
+    /**
+     * 失败
+     */
+    public static final boolean FAILED_BOOL = true;
+
+    /**
+     * 启用|禁用
+     */
+    public static final boolean ENABLE_BOOL = true;
+    public static final String ENABLE_STRING = "1";
+    public static final int ENABLE_INT = 1;
+
+    /**
+     * 禁用
+     */
+    public static final boolean DISABLE_BOOL = false;
+    public static final String DISABLE_STRING = "0";
+    public static final int DISABLE_INT = 0;
+
+    /**
+     * 常用数字 常量
+     */
+    public static final int ONE = 1;
+    public static final int TWO = 2;
+    public static final int THREE = 3;
 }
