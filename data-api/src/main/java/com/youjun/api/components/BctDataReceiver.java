29ff5a2a77e198826411fe34dd8e4a12bef80c80
xuzhipeng <xuzhipengaly>
2021-11-16 14:24:12 +0800

    mq修改

--- data-api/src/main/java/com/youjun/api/components/BctDataReceiver.java
+++ data-api/src/main/java/com/youjun/api/components/BctDataReceiver.java
@@ -6,7 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -18,11 +20,16 @@ import java.io.IOException;
  */
 @Slf4j
 @Component
-@RabbitListener(queues = "SWAP_REGULATION_QUEUE")
 public class BctDataReceiver {
     private static Logger log = LoggerFactory.getLogger(BctDataReceiver.class);
 
-    @RabbitHandler
+    @RabbitListener(bindings = {
+            @QueueBinding(
+                    value = @Queue(value = "SWAP_REGULATION_QUEUE", durable = "true"),
+                    exchange = @Exchange(value = "SWAP_REGULATION"),
+                    key = "swap.regulation")
+    })
+    //方式一
     public void handle(@Payload byte[] body, Message message, Channel channel) throws IOException {
         try {
             log.info("bct MQ 接收消息");
@@ -44,5 +51,27 @@ public class BctDataReceiver {
             channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
         }
     }
+    //方式二 body 等价于 message.getBody()
+    /*public void handle(Message message, Channel channel) throws IOException {
+        try {
+            log.info("bct MQ 接收消息");
+            String json = JsonUtils.mapper.readValue(message.getBody(), String.class);
+            log.info("json:{}", json);
+            if (message.getMessageProperties().getRedelivered()) {
+                log.error("消息已重复处理失败,拒绝再次接收...");
+                channel.basicReject(message.getMessageProperties().getDeliveryTag(), false);
+                //log.error("消息已重复处理失败,睡眠60s再次返回队列...");
+                //TimeUnit.SECONDS.sleep(60);
+                //log.info("睡眠结束");
+                //channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
+            }
+            log.info("已确认接收");
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            log.error("消息失败即将再次返回队列处理...");
+            channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
+        }
+    }*/
 
 }
--- data-api/src/main/java/com/youjun/api/schedule/AsyncDataScheduledTasks.java
+++ data-api/src/main/java/com/youjun/api/schedule/AsyncDataScheduledTasks.java
@@ -3,7 +3,6 @@ package com.youjun.api.schedule;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ExecutorService;
@@ -17,7 +16,7 @@ public class AsyncDataScheduledTasks {
     static private ExecutorService threadPool = Executors.newFixedThreadPool(10);
 
     @Async
-    @Scheduled(cron = "*/5 * * * * ?")
+//    @Scheduled(cron = "*/5 * * * * ?")
     public void dataAsync() {
         try {
             ApiDataDownloadRunnable apiDataDownloadRunnable = new ApiDataDownloadRunnable();
